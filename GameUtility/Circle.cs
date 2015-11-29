9af4d4d15d6221d11bfdd94543de832bd6127b6a
Paul Jones <shirlypunk@hotmail.com>
2015-11-28 20:57:33 -0500

    Added Circle v0.3.6

---
+++ GameUtility/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+
+namespace paujo.GameUtility {
+  public class Circle {
+
+    public Point Center {
+      get; set;
+    }
+
+    
+    public double Radius {
+      get; set;
+    }
+
+    
+    public double Area {
+      get {
+	return Math.PI * Radius * Radius;
+      }
+    }
+
+    
+    public Circle(Point center, double radius) {
+      this.Center = center;
+      this.Radius = radius;
+    }
+  }
+}
