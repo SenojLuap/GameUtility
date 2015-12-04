09d2aaedc7248a2942464972b2069a4e8d4c3438
Paul Jones <shirlypunk@hotmail.com>
2015-12-04 03:54:14 -0500

    Created ListRenderJob and InvisibleRenderJob v0.3.8

---
+++ GameUtility/InvisibleRenderJob.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace paujo.GameUtility {
+  public class InvisibleRenderJob : IRenderJob {
+    public void Draw(SpriteBatch spriteBatch) {
+      // Nothing
+    }
+
+    private InvisibleRenderJob() {
+    }
+
+    private static InvisibleRenderJob instance;
+
+    static InvisibleRenderJob() {
+      instance = new InvisibleRenderJob();
+    }
+
+    public static InvisibleRenderJob Instance() {
+      return instance;
+    }
+  }
+}
---
+++ GameUtility/ListRenderJob.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+using System.Collections.Generic;
+
+namespace paujo.GameUtility {
+  public class ListRenderJob {
+
+    public List<IRenderJob> List {
+      get; set;
+    }
+
+    public ListRenderJob() {
+      List = new List<IRenderJob>();
+    }
+
+    public void Add(IRenderJob newJob) {
+      List.Add(newJob);
+    }
+
+    public void Draw(SpriteBatch spriteBatch) {
+      foreach (var item in List)
+	item.Draw(spriteBatch);
+    }
+  }
+}
